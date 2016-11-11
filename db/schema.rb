4fd41de88bb0c931279a449020d3a9de11988c8c
Martin Rodriguez <Martin.Rodriquez@careerbuilder.com>
2016-11-11 15:36:25 -0300

    Follow relational model

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,16 @@ ActiveRecord::Schema.define(version: 20161120182830) do
     t.index ["user_id"], name: "index_meetings_on_user_id"
   end
 
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
+
   create_table "talks", force: :cascade do |t|
     t.integer  "meeting_id"
     t.string   "title",                      null: false
