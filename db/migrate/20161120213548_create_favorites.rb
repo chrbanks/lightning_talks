1f8252dc5e814f21e06dc7df1d4257d10747b23a
zwelchcb <Zachary.Welch@careerbuilder.com>
2016-11-20 21:10:53 -0500

    add unique index for favorites

--- db/migrate/20161120213548_create_favorites.rb
+++ db/migrate/20161120213548_create_favorites.rb
@@ -1,10 +1,14 @@
 class CreateFavorites < ActiveRecord::Migration[5.0]
   def change
     create_table :favorites do |t|
-      t.references :user, foreign_key: true, index: true
+      t.references :user, foreign_key: true
       t.references :talk, foreign_key: true
 
       t.timestamps
     end
+
+    add_index :favorites, :user_id
+    add_index :favorites, :talk_id
+    add_index :favorites, [:user_id, :talk_id], unique: true
   end
 end
